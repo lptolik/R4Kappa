41be954e747c8704e857223c6cb9a58a5e317258
A Sorokin <lptolik@gmail.com>
2015-03-03 17:14:53 +0300

    extract prepare.validation.project function to allow generation of validation project without execution

--- rkappa/R/write.kproject.R
+++ rkappa/R/write.kproject.R
@@ -64,10 +64,11 @@ write.kproject<-function(
   save(r4kproject,file=paste(projectdir,'/project.Rdat',sep=''))
 }
 
-validate.kproject<-function(
-  ###Prepare validate batch, write content of the \code{kproject} to the temp folder and run the project
+prepare.validation.project<-function(
+  ###Prepare project with validation batch
   kproject,##<<object to write
-  dir=tempdir(),##<<optional new destination for the writing
+  nrep=2,##<<optional number of repetitive evaluation calls to KaSim. 2 is recommended as it allows to check proper generation and invocation of simulation package
+  nsets=1,##<<optional number of test parameter sets to execute. In the case nsets > 1 validation is assumed to be a in concurrent mode.
   exe=kproject$execPath##<<optional local KaSim executable path to validate the model, if different from the execPath of the main project
 ){
   vproject<-kproject
@@ -75,8 +76,21 @@ validate.kproject<-function(
   vLines[13]<-"export KASIM_EXE=KKKKKK"
   vproject$shLines$validate<-vLines
   vproject$execPath=exe
-  vproject$nRep<-2
-  vproject$nSets<-2
+  vproject$nRep<-nrep
+  vproject$nSets<-nsets
+  return(vproject)
+  ###return the validation project
+}
+
+validate.kproject<-function(
+  ###Prepare validation batch, write content of the \code{kproject} to the temp folder and run the project
+  kproject,##<<object to write
+  dir=tempdir(),##<<optional destination directory to execute validation package
+  nrep=2,##<<optional number of repetitive evaluation calls to KaSim. 2 is recommended as it allows to check proper generation and invocation of simulation package
+  nsets=1,##<<optional number of test parameter sets to execute. In the case nsets > 1 validation is assumed to be a in concurrent mode.
+  exe=kproject$execPath##<<optional local KaSim executable path to validate the model, if different from the execPath of the main project
+){
+  vproject<-prepare.validation.project(kproject,nrep,nsets,exe)
   write.kproject(vproject,dir)
   cwd<-getwd()
   setwd(dir)
